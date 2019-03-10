beccb0ea58dfbd7c2d8b7a941d38e82d86986bf9
GitKangXiaolei <microkangxiaolei@lookout.com>
2019-03-09 19:58:18 -0500

    org.apache.axis:axis error!!

--- src/main/java/com/example/book/controller/TestController.java
+++ src/main/java/com/example/book/controller/TestController.java
@@ -4,11 +4,12 @@ import com.example.book.service.BorrowService;
 import com.example.book.service.UserService;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-@RestController
+@Controller
 public class TestController {
     @Autowired
     BorrowService borrowService;
